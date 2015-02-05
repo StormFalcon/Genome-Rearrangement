527006056c6e5f1699d9d141e2ffe7988d206e70
Wai Tsun Yim <flyerwaitsun@gmail.com>
2015-02-05 14:06:41 +0800

    - Added map method for Mapper() - map method still under construction

--- src/Mapper.java
+++ src/Mapper.java
@@ -1,4 +1,32 @@
 
 public class Mapper {
 
+	String originalSource;
+	String originalTarget;
+	String mappedSource;
+	String mappedTarget;
+	
+	public Mapper(String originalSource, String originalTarget)
+	{
+		this.originalSource = originalSource;
+		this.originalTarget = originalTarget;
+		Pair pair = map();
+		this.mappedSource = pair.source;
+		this.mappedTarget = pair.target;
+	}
+
+	/**
+	 * STILL UNDER CONSTRUCTION
+	 * @return
+	 */
+	public Pair map()
+	{
+		String newSource = null;
+		String newTarget = null;
+		
+		Pair pair = new Pair(newSource, newTarget);
+		
+		return pair;
+	}
+	
 }
